4e81f7f868de085889d04d3a96f6d12d86987447
04603aslan <baris_karapelit_298@hotmail.com>
2019-10-25 22:31:10 +0300

    Java deneme Dosyası

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*int a=12;
+	int b=89;*/
+	/*if(a==b)
+    {
+        System.out.println("A sayısı B sayısına eşit");
+
+    }else if(a<=b)
+    {
+        System.out.println("A küçük veya eşit B ye ");
+    }else if(a>=b)
+    {
+        System.out.println("A büyük veya eşit B");
+    }*/
+
+       /* int age = 23;
+        String sex = "woman";
+        if (age == 23 && sex == "man") {
+            System.out.println("Sonuç doğru");
+        } else {
+
+
+            System.out.println("Result isn't True");
+
+        }*/
+
+    /*int a=8;
+    int b=14;
+        System.out.println((a<b)?a:b);
+*/
+  /*  long c=17L;
+    byte a=12;
+    int b=25;
+    a=(byte) (a+b);
+        a =(byte) (a+c);
+        System.out.println(a);*/
+/*
+
+        float a= (float) 18.3;
+        short b=12;
+        double c=12.5;
+
+
+*/
+        String enter="89";
+        int a=Integer.valueOf(enter);
+        System.out.println(a);
+
+
+    }
+
+}
